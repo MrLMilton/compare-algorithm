07e1c13914f66570704e80229f8b1fe535bab5a2
Lang Milton <lmilton@heckgrammar.co.uk>
2025-01-15 14:28:27 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Diagnostics;
+
+namespace compare_algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random n = new Random();
+            Stopwatch sw = new Stopwatch();
+
+
+        }
+        static int[] CreateArray(int size, Random r)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = r.Next(1000, 100000);
+            }
+            return array;
+        }
+        static void menu()
+        {
+
+        }
+        
+        static void BubbleSort(int[] a)
+        {
+            
+        }
+        static void Merge(int[] a, int low, int mid, int high)
+        {
+            int i, j, k;
+            int num1 = mid - low + 1;
+            int num2 = high - mid;
+            int[] L = new int[num1];
+            int[] R = new int[num2];
+            for (i = 0; i < num1; i++)
+            {
+                L[i] = a[low + i];
+            }
+            for (j = 0; j < num2; j++)
+            {
+                R[j] = a[mid + j + 1];
+            }
+            i = 0;
+            j = 0;
+            k = low;
+            while (i < num1 && j < num2)
+            {
+                if (L[i] <= R[j])
+                {
+                    a[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    a[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < num1)
+            {
+                a[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < num2)
+            {
+                a[k] = R[j];
+                j++; k++;
+            }
+        }
+        static void MergeSortRecursive(int[] a, int low, int high)
+        {
+            if (low < high)
+            {
+                int mid = (low + high) / 2;
+                MergeSortRecursive(a, low, mid);
+                MergeSortRecursive(a, mid + 1, high);
+                Merge(a, low, mid, high);
+            }
+        }
+        static bool LinearSearchIfFound(int[] a, int numToFind)
+        {
+
+        }
+        static bool BinarySearch(int[] a, int numToFind)
+        {
+            
+        }
+            
+    }
+}
